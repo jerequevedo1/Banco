0fab0c7fabc494b70097a9ec5e456a4fe90cc3f5
julipoltenguerci <jpoltenguerci@gmail.com>
2021-10-27 21:23:50 -0300

    Se agrega carpeta con archivos de BD

---
+++ BaseDatos/DB_Banco_JJRG.sql
@@ -0,0 +1,96 @@
+--Creación de la base de datos "Banco JJRG"
+create database BancoJJRG
+go
+use BancoJJRG
+go
+create table Provincias
+(
+	id_provincia int identity(1,1) not null,
+	nom_provincia varchar(35),
+	constraint pk_id_provincia primary key(id_provincia)
+)
+go
+create table Localidades
+(
+	id_localidad int identity(1,1) not null,
+	nom_localidad varchar(35),
+	id_provincia int,
+	constraint pk_id_localidad primary key(id_localidad),
+	constraint fk_id_provincia foreign key (id_provincia) references Provincias(id_provincia)
+)
+go
+create table Barrios
+(
+	id_barrio int identity(1,1) not null,
+	nom_barrio varchar(40),
+	id_localidad int,
+	constraint pk_id_barrio primary key(id_barrio),
+	constraint fk_id_localidad foreign key (id_localidad) references Localidades(id_localidad)
+)
+go
+create table Tipos_Cuentas
+(
+	id_tipo_cuenta int identity(1,1) not null,
+	descripcion varchar(80),
+	constraint pk_id_tipo_cuenta primary key(id_tipo_cuenta)
+)
+go
+create table Tipos_Transacciones
+(
+	id_tipo_transac int identity(1,1) not null,
+	descripcion varchar(80),
+	constraint pk_id_tipo_transac primary key(id_tipo_transac)
+)
+go
+create table Usuarios
+(
+	id_usuario int identity(1,1) not null,
+	usuario varchar(50),
+	contraseña varchar(18),
+	constraint pk_id_usuario primary key(id_usuario)
+)
+go
+create table Clientes
+(
+	id_cliente int identity(1,1) not null,
+	nom_cliente varchar(50),
+	ape_cliente varchar(50),
+	dni int,
+	cuil bigint,
+	direccion varchar(100),
+	telefono varchar(30),
+	email varchar(80),
+	id_barrio int,
+	constraint pk_id_cliente primary key (id_cliente),
+	constraint fk_id_clientes_barrio foreign key (id_barrio) references Barrios(id_barrio)
+)
+go
+create table Cuentas 
+(
+	id_cuenta int identity(1,1) not null,
+	cbu varchar(22),
+	alias varchar(20),
+	saldo_actual decimal,
+	ultimo_movimiento int,
+	saldo_en_descubierto decimal,
+	id_cliente int,
+	id_tipo_cuenta int,
+	tipo_moneda char(1),
+	constraint pk_id_cuenta primary key (id_cuenta),
+	constraint fk_id_cta_cliente foreign key (id_cliente) references Clientes(id_cliente),
+	constraint fk_id_cta_tipo_cta foreign key (id_tipo_cuenta) references Tipos_Cuentas(id_tipo_cuenta)
+)
+go
+create table Transacciones 
+(
+	id_transaccion int identity(1,1) not null,
+	fecha datetime,
+	monto decimal,
+	id_tipo_transac int,
+	id_cuenta int,
+	constraint pk_id_transaccion primary key (id_transaccion),
+	constraint fk_id_transac_tipo_transac foreign key (id_tipo_transac) references Tipos_Transacciones(id_tipo_transac),
+	constraint fk_id_transac_cta foreign key (id_cuenta) references Cuentas(id_cuenta)
+)
+go
+
---
+++ BaseDatos/Datos_Banco_JJRG.sql
@@ -0,0 +1,137 @@
+-- Datos Insertados - BD Farmacéutica
+
+--Provincias
+
+Insert into Provincias (nom_provincia) values ('Córdoba')
+Insert into Provincias (nom_provincia) values ('San Luis')
+Insert into Provincias (nom_provincia) values ('Santa Fe')
+Insert into Provincias (nom_provincia) values ('Buenos Aires')
+
+--Select *
+--From Provincias
+
+--Localidades
+
+Insert into Localidades (id_provincia, nom_localidad) values (1,'Córdoba Capital')
+Insert into Localidades (id_provincia, nom_localidad) values (1, 'Villa Allende')
+Insert into Localidades (id_provincia, nom_localidad) values (1, 'Alta Gracia')
+Insert into Localidades (id_provincia, nom_localidad) values (1, 'Mendiolaza')
+
+Insert into Localidades (id_provincia, nom_localidad) values (2,'San Luis Capital')
+Insert into Localidades (id_provincia, nom_localidad) values (2, 'Villa Mercedes')
+Insert into Localidades (id_provincia, nom_localidad) values (2, 'Villa de Merlo')
+Insert into Localidades (id_provincia, nom_localidad) values (2, 'La Punta')
+
+Insert into Localidades (id_provincia, nom_localidad) values (3,'Santa Fe Capital')
+Insert into Localidades (id_provincia, nom_localidad) values (3, 'Rosario')
+Insert into Localidades (id_provincia, nom_localidad) values (3, 'Rafaela')
+Insert into Localidades (id_provincia, nom_localidad) values (3, 'Sunchales')
+
+Insert into Localidades (id_provincia, nom_localidad) values (4,'CABA')
+Insert into Localidades (id_provincia, nom_localidad) values (4, 'La Plata')
+Insert into Localidades (id_provincia, nom_localidad) values (4, 'Avellaneda')
+Insert into Localidades (id_provincia, nom_localidad) values (4, 'Morón')
+
+--Select *
+--From Localidades
+
+--Barrios
+
+Insert into Barrios (id_localidad, nom_barrio) values (1, 'Centro')
+Insert into Barrios (id_localidad, nom_barrio) values (1, 'Alberdi')
+Insert into Barrios (id_localidad, nom_barrio) values (1, 'Nueva Córdoba')
+Insert into Barrios (id_localidad, nom_barrio) values (1, 'Cerro de las Rosas')
+Insert into Barrios (id_localidad, nom_barrio) values (2, 'San Isidro')
+Insert into Barrios (id_localidad, nom_barrio) values (2, 'San Martín')
+Insert into Barrios (id_localidad, nom_barrio) values (3, 'Parque del Virrey')
+Insert into Barrios (id_localidad, nom_barrio) values (3, 'Portales del Sol')
+Insert into Barrios (id_localidad, nom_barrio) values (4, 'El Talar')
+
+Insert into Barrios (id_localidad, nom_barrio) values (5, 'Unión')
+Insert into Barrios (id_localidad, nom_barrio) values (6, 'Jardín del Sur')
+Insert into Barrios (id_localidad, nom_barrio) values (7, 'Barranca Colorada')
+Insert into Barrios (id_localidad, nom_barrio) values (8, 'La Candelaria')
+
+Insert into Barrios (id_localidad, nom_barrio) values (9, 'Sur')
+Insert into Barrios (id_localidad, nom_barrio) values (9, 'Centro')
+Insert into Barrios (id_localidad, nom_barrio) values (10, 'Parque')
+Insert into Barrios (id_localidad, nom_barrio) values (10, 'Abasto')
+Insert into Barrios (id_localidad, nom_barrio) values (11, '2 de Abril')
+Insert into Barrios (id_localidad, nom_barrio) values (11, 'Barranquitas')
+Insert into Barrios (id_localidad, nom_barrio) values (12, 'Colón')
+
+Insert into Barrios (id_localidad, nom_barrio) values (13, 'Almagro')
+Insert into Barrios (id_localidad, nom_barrio) values (13, 'Parque Avellaneda')
+Insert into Barrios (id_localidad, nom_barrio) values (14, 'Carmen Oeste')
+Insert into Barrios (id_localidad, nom_barrio) values (15, 'Sarandí')
+Insert into Barrios (id_localidad, nom_barrio) values (16, 'Santa Laura')
+
+--Select *
+--From Barrios
+
+--Usuarios
+
+Insert into Usuarios (usuario, contraseña) values ('jpoltenguerci', 'C123456')
+Insert into Usuarios (usuario, contraseña) values ('jquevedo', '102030$')
+Insert into Usuarios (usuario, contraseña) values ('gmedrano', '405060$')
+Insert into Usuarios (usuario, contraseña) values ('rmedina', '708090$')
+
+--Select *
+--From Usuarios
+
+--Tipos de Cuentas
+
+Insert into Tipos_Cuentas (descripcion) values ('Caja de Ahorro')
+Insert into Tipos_Cuentas (descripcion) values ('Cuenta Corriente')
+Insert into Tipos_Cuentas (descripcion) values ('Cuenta Sueldo')
+
+--Select *
+--From Tipos_Cuentas
+
+--Tipos de Transacciones
+
+Insert into Tipos_Transacciones (descripcion) values ('Depósito')
+Insert into Tipos_Transacciones (descripcion) values ('Transferencia')
+Insert into Tipos_Transacciones (descripcion) values ('Acreditación')
+Insert into Tipos_Transacciones (descripcion) values ('Acreditación de haberes')
+Insert into Tipos_Transacciones (descripcion) values ('Pago de Servicio')
+Insert into Tipos_Transacciones (descripcion) values ('Extracción Por Cajero')
+
+--Select *
+--From Tipos_Transacciones
+
+--Clientes
+
+Insert into Clientes (nom_cliente, ape_cliente, dni, cuil, direccion, telefono, email, id_barrio) values ('Rosalia', 'Suarez', 33567844, 27335678446, 'Tucuman 348', '3516598223', 'rosisuarez@gmail.com', 1)
+Insert into Clientes (nom_cliente, ape_cliente, dni, cuil, direccion, telefono, email, id_barrio) values ('Francisco', 'Flores', 28733499, 20287334995, 'San Martin 1145', '3515451109', 'fflores@hotmail.com', 10)
+Insert into Clientes (nom_cliente, ape_cliente, dni, cuil, direccion, telefono, email, id_barrio) values ('Ismael', 'Rodriguez', 30045283, 20300452837, 'Rodriguez del Busto 2040', '3512498709', 'rodiguezismael05@hotmail.com', 4)
+
+--Select *
+--From Clientes
+
+--Cuentas
+
+Insert into Cuentas (cbu, alias, saldo_actual, ultimo_movimiento, saldo_en_descubierto, id_cliente, id_tipo_cuenta, tipo_moneda) values ('0200557341000007712948', 'rosalia.suarez', 34785, 5, 0, 1, 3, 'P')
+Insert into Cuentas (cbu, alias, saldo_actual, ultimo_movimiento, saldo_en_descubierto, id_cliente, id_tipo_cuenta, tipo_moneda) values ('0200746241000006183303', 'fran.flores', 12790, 4, 0, 2, 1, 'P')
+
+
+--Select *
+--From Cuentas
+
+--Transacciones
+
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('10/05/2021', 105000, 4, 1)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('12/05/2021', 5000, 5, 1)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('12/05/2021', 15200, 2, 1)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('27/05/2021', 25000, 5, 1)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('02/06/2021', 105000, 5, 1)
+
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('24/07/2021', 14500, 3, 2)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('04/08/2021', 97000, 3, 2)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('19/08/2021', 30000, 3, 2)
+Insert into Transacciones (fecha, monto, id_tipo_transac, id_cuenta) values ('20/08/2021', 20000, 1, 2)
+
+
+--Select *
+--From Transacciones
+
