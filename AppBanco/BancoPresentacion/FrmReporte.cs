d474901a0141fac8e5053674deb4f896a89085ca
jerequevedo1 <86841866+jerequevedo1@users.noreply.github.com>
2021-11-02 15:33:47 -0300

    correccion reportes

--- AppBanco/BancoPresentacion/FrmReporte.cs
+++ AppBanco/BancoPresentacion/FrmReporte.cs
@@ -25,18 +25,28 @@ namespace BancoPresentacion
         {
             reportViewer1.ProcessingMode = ProcessingMode.Local;
             string appFolder = Path.GetDirectoryName(Application.StartupPath);
-            reportViewer1.LocalReport.ReportPath = @"C:\Users\jpolt\OneDrive\Escritorio\Banco_JJRS\AppBanco\BancoPresentacion\Reportes\RptCuentasPorCliente.rdlc";
+            var rutaRpt = Path.Combine(Application.StartupPath, @"~/Reportes\RptCuentasPorCliente.rdlc");
+
+
+            string pathAbosluto = Application.StartupPath;
+            string fullPath = Path.Combine(pathAbosluto, @"..\..\..\Reportes\RptCuentasPorCliente.rdlc");
+            fullPath = Path.GetFullPath(fullPath);
+
+            reportViewer1.LocalReport.ReportPath = fullPath;
+
+            //reportViewer1.LocalReport.ReportPath = @"C:\Users\jpolt\OneDrive\Escritorio\Banco_JJRS\AppBanco\BancoPresentacion\Reportes\RptCuentasPorCliente.rdlc";
             reportViewer1.Refresh();
-            
+
+
             DataSetCuentasCliente ds = new DataSetCuentasCliente();
 
             var datos = HelperDao.ObtenerInstancia().ConsultaSQL("PA_REPORTE_CUENTAS_CLIENTE");
             //DATASOURCE
             ReportDataSource reportDataSource = new ReportDataSource("DataSet1", datos);
-           
+
             this.reportViewer1.LocalReport.DataSources.Add(reportDataSource);
             reportViewer1.Refresh();
-     
+
         }
 
         private void FrmReporte_Load(object sender, EventArgs e)
